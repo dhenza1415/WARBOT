447770b7ff031ca4229ddc9e1da93978d8cbc1db
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2018-12-27 00:26:09 +0700

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,72 @@
+"""This All bots script
+made by arsybai pengembang 👉⚔TBP Sɪʟᴇɴᴛᵏᶦˡˡᵉʳ.
+Do not use it without permission"""
+class helper:
+    """This For 
+    Help Send MSG"""
+    def __init__(self):
+        """Whatever I want
+        I just make it
+        for myself LOL"""
+        pass
+    
+    def mainHelp(self):
+        thishelp = """[ HELPER ]
+>SIMPLE SELF BY ⚔TBP Sɪʟᴇɴᴛᵏᶦˡˡᵉʳ
+> Self
+> Group
+> Media
+> Settings
+
+© 2018.SELF V.10"""
+        return(thishelp)
+        
+    def selfHelp(self):
+        thishelp = """[ SELF ]
+> Me
+> Clone [@]
+> Restore
+> Grouplist
+> Friendlist
+> Add [@]"""
+        return(thishelp)
+        
+    def groupHelp(self):
+        thishelp = """[ GROUP ]
+> Gmember
+> Ginfo
+> Tagall
+> Kick [@]
+> Reinvite [@]"""
+        return(thishelp)
+        
+    def setHelp(self):
+        thishelp = """[ SETTINGS ]
+> Autoadd [on/off]
+> Autojoin [on/off]
+> Autolike [on/off]
+> Autocomment [on/off]
+> Changepicture
+> Speedcheck
+> Restart
+> Shutdown"""
+        return(thishelp)
+        
+    def mediaHelp(self):
+        thishelp = """[ MEDIA ]
+> coming soon
+> coming soon
+> coming soon
+> coming soon
+> coming soon
+> coming soon
+> coming soon"""
+        return(thishelp)
+        
+        
+        
+    
+    
+    
+    
+    
